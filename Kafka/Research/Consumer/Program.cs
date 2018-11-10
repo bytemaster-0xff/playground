35f68ba2f8c6239ccdbf91d32ef24f5107027d97
Kevin D. Wolf <kevinw@software-logistics.com>
2018-11-10 09:04:01 -0500

    add kafka

---
+++ Kafka/Research/Consumer/Program.cs
@@ -0,0 +1,64 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Consumer
+{
+    class Program
+    {
+        private static void Consume()
+        {
+
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "kafka-dev.iothost.net:9092",
+                 BrokerAddressFamily = BrokerAddressFamilyType.V4,
+                  Debug = "consumer",
+                   
+                // Note: The AutoOffsetReset property determines the start offset in the event:
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // eariest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetResetType.Earliest
+            };
+
+            using (var c = new Consumer<Ignore, string>(conf))
+            {
+                c.Subscribe("test");
+                Console.WriteLine("Starting Consumption");
+
+                bool consuming = true;
+                // The client will automatically recover from non-fatal errors. You typically
+                // don't need to take any action unless an error is marked as fatal.
+                c.OnError += (_, e) =>
+                {
+                    Console.WriteLine(e.Reason);
+                    consuming = !e.IsFatal;
+                };
+
+                while (consuming)
+                {
+                    try
+                    {
+                        var cr = c.Consume();
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+
+                // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                c.Close();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Consume();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Kafka/Research/Producer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Producer
+{
+    class Program
+    {
+        static async void Send()
+        {
+            Console.WriteLine("Sending...");
+
+            var config = new ProducerConfig { BootstrapServers = "kafka-dev.iothost.net:9092" };
+
+            // A Producer for sending messages with null keys and UTF-8 encoded values.
+            using (var p = new Producer<Null, string>(config))
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("test", new Message<Null, string> { Value = "Hello World" + Guid.NewGuid().ToString() });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (KafkaException e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Running - X to Stop");
+
+            bool running = true;
+            while(running)
+            {
+               if( Console.ReadKey().Key == ConsoleKey.X)
+                {
+                    running = false;
+                }
+                else
+                {
+                    Send();
+                }
+            }
+        }
+    }
+}
